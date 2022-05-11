975b522ad6a5ed82b3a5625d106a9aa63b6174b3
javygimenez <soy_entrerriano@hotmail.com>
2022-05-11 14:55:40 -0300

    commit02

---
+++ src/Navbar.js
@@ -0,0 +1,27 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div className="container-fluid">
+  <a className="navbar-brand" href="#">
+      <img src="./public/logo_el_envase.png" alt="" width="30" height="24"></img>         
+    </a>
+    <a className="navbar-brand" href="#"><h1>EL ENVASE</h1></a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div className="navbar-nav">
+        <a className="nav-link" aria-current="page" href="#">INICIO</a>
+        <a className="nav-link" href="#">NOSOTROS</a>
+        <a className="nav-link" href="#">PRODUCTOS</a>
+        <a className="nav-link" href="#">CARRITO</a>
+      </div>
+    </div>
+  </div>
+</nav>
+    );
+}
+
+export default Navbar;
